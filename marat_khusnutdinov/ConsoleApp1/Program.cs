bb32f9915d6f28e5de307c8e3a7b6251943da744
Black Berry <marat4800@gmail.com>
2019-10-24 20:43:42 +0300

    Create task number five

---
+++ marat_khusnutdinov/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] inputarray = input.ToCharArray();
+            Array.Reverse(inputarray);
+            string output = new string(inputarray);
+            Console.WriteLine(output);
+        }
+    }
+}
